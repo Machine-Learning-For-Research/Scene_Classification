f3e73ad22f3411136450b695c718b7a792b5e1ff
fuwinterdong <fuwinterdong@gmail.com>
2017-11-23 14:57:31 +0800

    inception-resnet-v2

--- classifier.py
+++ classifier.py
@@ -1,69 +1,108 @@
 # -*- coding: utf-8 -*-
 #!/user/bin/env python3
 import os
+import h5py
 import keras
 import numpy as np
+import tensorflow as tf
 from keras.preprocessing.image import ImageDataGenerator
-from keras.callbacks import ModelCheckpoint
-from keras.layers import Conv2D, MaxPooling2D
+from keras.callbacks import ModelCheckpoint, TensorBoard
+from keras.layers import Conv2D, MaxPooling2D, GlobalAveragePooling2D
 from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.models import Sequential
-from keras.optimizers import SGD
+from keras.models import Sequential, Model
 from keras.applications.vgg16 import VGG16
-from keras.optimizers import Adam, SGD
+from keras.applications.resnet50 import ResNet50
+from keras.applications.xception import Xception, preprocess_input
+from keras.applications.inception_resnet_v2 import InceptionResNetV2, preprocess_input
+from keras.optimizers import Adam, SGD, RMSprop, Nadam
 from keras import initializers
+from keras.layers.normalization import BatchNormalization
+#from keras.utils import multi_gpu_model
 #%% Parameters
-train_data_dir = '/home/winter/Keras/SceneClassify/data/train/train_data'
-validation_data_dir = '/home/winter/Keras/SceneClassify/data/validation/validation_data'
-path_weights = 'params/first_try.h5'
-
-img_width, img_height = 500, 500
+train_data_dir = '/home/winter/Keras/SceneClassify_Winter/data/train/train_data'
+validation_data_dir = '/home/winter/Keras/SceneClassify_Winter/data/validation/validation_data'
+path_weights = 'params/inception_resnet_v2/{epoch:05d}-{val_loss:.4f}-{val_acc:.4f}.h5'
+path_summary = 'logs/'
+img_width, img_height = 299, 299
 batch_size = 32
 nb_train_samples = 53879
 nb_validation_samples = 7210
 epochs = 50
+nb_classes =80
+#%%
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+nadam = Nadam(lr=0.002, beta_1 = 0.9, beta_2=0.999, epsilon=1e-8, schedule_decay=0.004)
+#  add new layers
+def add_new_last_layer(base_model, nb_classes):
+  """
+  input: base_model and clasess
+  ouput: new keras model
+  """
+  x = base_model.output
+  x = GlobalAveragePooling2D()(x)
 
+  predictions = Dense(nb_classes, activation='softmax')(x) #new softmax layer
+  model = Model(inputs=base_model.input, output=predictions)
+  model.summary()
+  return model
+
+# free the base_model layers
+def setup_to_transfer_learn(model, base_model):
+  """Freeze all layers and compile the model"""
+  for layer in base_model.layers:
+      layer.trainable = False
+  model.compile(loss='categorical_crossentropy',
+              optimizer='nadam',
+              metrics=['accuracy'])
+    
+ 
 #%% bulid model
-#model = VGG16(include_top=True, weights=None, input_tensor=None, input_shape=None,
-#              pooling=None, classes = 80)
+#base_model = VGG16(include_top=False, weights='imagenet', input_tensor=None) # download no_top model
+#base_model = Xception(include_top=False, weights='imagenet')
+base_model = InceptionResNetV2(include_top=False, weights='imagenet')
+model = add_new_last_layer(base_model, nb_classes)
+setup_to_transfer_learn(model, base_model)
+
+#base_model = ResNet50(include_top=False, weights=path_weights, input_tensor=None, input_shape=None, pooling=None, 
+#                 classes=80)
+#
+#base_model = InceptionResNetv2(include_top=False, weights=path_weights, input_tensor=None, input_shape=None, pooling=None, 
+#                 classes=80)
+
 
 ################################sequential to build model#################################
-model = Sequential()
-model.add(Conv2D(32, (3, 3), activation= 'relu', input_shape=(500, 500, 3)))
-model.add(Conv2D(32, (3, 3), activation= 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Conv2D(64, (3, 3), activation= 'relu'))
-model.add(Conv2D(64, (3, 3), activation= 'relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-
-model.add(Flatten())
-model.add(Dense(256, activation='relu'))
-model.add(Dropout(0.5))
-model.add(Dense(80, activation='softmax'))
-
-
+#with tf.device('/cpu:0'):
+#model = Sequential()
+##################################################################################
 #adam = Adam(lr=1e-4,  beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss='categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+#sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+#parallel_model = multi_gpu_model(model, gpus=2)
+#rmsprop = RMSprop(lr = 0.01, rho=0.9, epsilon=1e-8)
+#model.compile(loss='categorical_crossentropy',
+#              optimizer='sgd',
+#              metrics=['accuracy'])
 
 #%% train and validaton
-train_datagen = ImageDataGenerator(
-    rescale=1. / 255,
-    shear_range=0.0,
-    zoom_range=0.0,
-    horizontal_flip=True)
+train_datagen = ImageDataGenerator(#rescale=1. / 255,
+                                   channel_shift_range=0.1,
+                                   rotation_range=15,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='reflect',
+                                   preprocessing_function=preprocess_input)
 
-test_datagen = ImageDataGenerator(rescale=1. / 255)
+test_datagen = ImageDataGenerator(#rescale=1. / 255,
+                                  fill_mode='reflect',
+                                  preprocessing_function=preprocess_input)
 
 train_generator = train_datagen.flow_from_directory(
     train_data_dir,
     target_size=(img_width, img_height),
     batch_size=batch_size,
+    shuffle=True,
     class_mode='categorical')
 
 validation_generator = test_datagen.flow_from_directory(
@@ -71,7 +110,7 @@ validation_generator = test_datagen.flow_from_directory(
     target_size=(img_width, img_height),
     batch_size=batch_size,
     class_mode='categorical')
-
+#%%
 if os.path.exists(path_weights):
     model.load_weights(path_weights)
     print('Starting train with %s ' % path_weights)
@@ -81,7 +120,8 @@ else:
 model.fit_generator(
     train_generator,
     steps_per_epoch=nb_train_samples // batch_size,
-    callbacks = [ModelCheckpoint(path_weights)],
+    callbacks = [ModelCheckpoint(path_weights,),
+                 TensorBoard(log_dir= path_summary)], #every epoch save model
     epochs=epochs,
     validation_data=validation_generator,
     validation_steps=nb_validation_samples // batch_size)
